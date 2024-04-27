31d2e6de773bf2b480f188df00be66822b7548e8
Cesar Lechuga <cesaryael2004@hotmail.com>
2024-04-26 21:53:43 -0600

    version 1

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona {
+    String nombre,genero;
+    int edad; 
+    double altura;
+    public void mostrarNombre(){
+        System.out.println("mi nombre es "+this.nombre);
+    }
+    public void mostrarEdad(){
+        System.out.println("tengo: "+ this.edad +" años");
+    }
+    public void mostrarAltura(){
+        System.out.println("mi altura "+this.altura);
+    }
+    
+}
